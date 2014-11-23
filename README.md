424d4cbb1689498cd93105e324077d3f0e26bb86
Scott M <smcmillan@sei.cmu.edu>
2014-11-23 15:27:25 -0500

    First working version of script.  Updated code book (work in progress)

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 jhu_getdata_009
 ===============
 
-Course project for JHU Getting and Cleaning Data course
+Course project for JHU Getting and Cleaning Data course.  Data for this project comes from the UCI Machine Learning Repository and is the Human Activity Recognition Using Smartphones Data Set found at the following URL:
+
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+Code Book
+---------
+
+From the complete feature list of over 500 measurements, I have extracted the following mean and standard deviation features (original dataset feature indices provided), to process for this assignment:
+
+| New Indices | Original Indices | Description                          |
+| ----------- | ---------------- | ------------------------------------ |
+| 1  |  | Test subject ID
+| 2  |  | Activity label (one of "WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING")
+|  | 1:6     |  tBodyAcc mean and std for X, Y, and Z axes
+|  | 41:46   |  tGravityAcc mean and std for X, Y, and Z axes
+|  | 81:86   |  tBodyAccJerk mean and std for X, Y, and Z axes
+|  | 121:126 |  tBodyGyro mean and std for X, Y, and Z axes
+|  | 161:166 |  tBodyGyroJerk mean and std for X, Y, and Z axes
+|  | 201,202 |  tBodyAccMag mean and std of the magnitude
+|  | 214,215 |  tGravityAccMag mean and std of the magnitude
+|  | 227,228 |  tBodyAccJerkMag mean and std of the magnitude
+|  | 240,241 |  tBodyGyroMag mean and std of the magnitude
+|  | 253,254 |  tBodyGyroJerkMag mean and std of the magnitude
+|  | 266:271 |  fBodyAcc mean and std for X, Y, and Z axes
+|  | 345:350 |  fBodyAccJerk mean and std for X, Y, and Z axes
+|  | 424:429 |  fBodyGyro mean and std for X, Y, and Z axes
+|  | 503,504 |  fBodyAccMag mean and std of the magnitude
+|  | 516,517 |  fBodyBodyAccJerkMag mean and std of the magnitude
+|  | 529,530 |  fBodyBodyGryoMag mean and std of the magnitude
+|  | 542,543 |  fBodyBodyGryoJerkMag mean and std of the magnitude
--- run_analysis.R
+++ run_analysis.R
@@ -1,64 +1,76 @@
-## ---------------------------------------------------------------------
+## -----------------------------------------------------------------------
 ## This function cleans the UCI HAR Dataset.
 ##
 ## Return: A "tidy" data set with the means of all mean and std
-##         measurements aggregated for each subject and activity.
+##         measurements aggregated by each unique subject and activity
+##         pair.  Note, the field names are the same as the original data
+##         that goes into calculate the mean.
 ##
 ## Note: run this script with the current working directory set to
 ##       the root "UCI HAR Dataset"
-## ---------------------------------------------------------------------
-library(dplyr)
-library(tidyr)
-
-runAnalysis <- function() {
-	setwd(dirname)
-
-	# Merge the training and test sets
-	testX <- read.table("test/X_test.txt")
-	trainX <- read.table("train/X_train.txt")
-	featuresX <- rbind(trainX, testX)
-
+## -----------------------------------------------------------------------
+run_analysis <- function() {
+	#----------------------------------------------------
+	# Merge the training and test activity labels
 	testy <- read.table("test/y_test.txt")
 	trainy <- read.table("train/y_train.txt")
 	activityIndex <- rbind(trainy, testy)
 
+	#----------------------------------------------------
+	# Merge the training and test subjects
 	testSubjects <- read.table("test/subject_test.txt")
 	trainSubjects <- read.table("train/subject_train.txt")
 	subjects <- rbind(trainSubjects, testSubjects)
 
-	# set the feature names
+	#----------------------------------------------------
+	# Merge the training and test feature sets and load the feature names
+	testX <- read.table("test/X_test.txt")
+	trainX <- read.table("train/X_train.txt")
+	featuresX <- rbind(trainX, testX)
+
+	# Load the feature names
 	featureNames <- read.table("features.txt")
 	colnames(featuresX)<-featureNames[,2]
 
-	# set the label name and convert indicies to descriptive activity factor
-	activityLabels <- read.table("activity_labels.txt")
-	activity <- activityLabels[activityIndex[,1],2]
-
-	# Extract just the mean and stddev features
+	# Extract the 68 mean and stddev measurement features from the original data.
+	# One could add more indicies here to add the "*Means" features if needed.
 	fieldIndices = c(
-		1:6,      #tBodyAcc
-            41:46,    #tGravityAcc
-		81:86,    #tBodyAccJerk
-		121:126,  #tBodyGyro
-            161:166,  #tBodyGyroJerk
-		201,202,  #tBodyAccMag
-		214,215,  #tGravityAccMag
-		227,228,  #tBodyAccJerkMag
-		240,241,  #tBodyGyroMag
-		253,254,  #tBodyGyroJerkMag
-		266:271,  #fBodyAcc
-		345:350,  #fBodyAccJerk
-		424:429,  #fBodyGyro
-		503,504,  #fBodyAccMag
-		516,517,  #fBodyBodyAccJerkMag
-		529,530,  #fBodyBodyGryoMag
-		542,543)  #fBodyBodyGryoJerkMag
+		1:6,      # tBodyAcc X,Y,Z
+            41:46,    # tGravityAcc X,Y,Z
+		81:86,    # tBodyAccJerk X,Y,Z
+		121:126,  # tBodyGyro X,Y,Z
+            161:166,  # tBodyGyroJerk X,Y,Z
+		201,202,  # tBodyAccMag
+		214,215,  # tGravityAccMag
+		227,228,  # tBodyAccJerkMag
+		240,241,  # tBodyGyroMag
+		253,254,  # tBodyGyroJerkMag
+		266:271,  # fBodyAcc X,Y,Z
+		345:350,  # fBodyAccJerk X,Y,Z
+		424:429,  # fBodyGyro X,Y,Z
+		503,504,  # fBodyAccMag
+		516,517,  # fBodyBodyAccJerkMag
+		529,530,  # fBodyBodyGryoMag
+		542,543)  # fBodyBodyGryoJerkMag
 	
-	dataset <- featuresX[,fieldIndices]
-	dataset$Activity = activity
-	dataset$Subjects = subjects$V1
+	# build a data frame to perform aggregate mean calculations
+	mean_std_measurements <-NULL
+	mean_std_measurements$Subject <- subjects$V1
+	mean_std_measurements$Activity <- activityIndex$V1
+	mean_std_measurements <- cbind(mean_std_measurements, featuresX[,fieldIndices])
 
-	# Aggregate data for each type of activity
-	subset1 = subset(dataset, Activity = "LAYING")
-	subset2 = subset(dataset, Activity = "SITTING"
-	 "STANDING" "WALKING" "WALKING_DOWNSTAIRS" "WALKING_UPSTAIRS"
+	# compute the mean of each column of data in each subset group by subject and activity
+	measurementMeans <- aggregate(mean_std_measurements,
+						by=list(Subject,Activity),
+						FUN = "mean")
+
+	# replace the activity indices with descriptive factors (maintain the original order)
+	activityLabels <- read.table("activity_labels.txt")
+	measurementMeans$Activity <- factor(measurementMeans$Activity, labels=activityLabels[,2])
+
+	# Remove the unnecessary Group "by" variables added by the aggregate function.
+	measurementMeans$Group.1 <- NULL
+	measurementMeans$Group.2 <- NULL
+	
+	measurementMeans
+}
